a87f4f3deb1569298369fc7fba0dd74541c27522
cmj <chmin82@gmail.com>
2021-01-29 17:37:24 +0900

    session 쿠키 vs permanent 쿠키

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -8,7 +8,11 @@ http.createServer((request, response) => {
   };
   console.log(cookies.yummy_cookie);
   response.writeHead(200, {
-    'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    'Set-Cookie':[
+      'yummy_cookie=choco',
+      'tasty_cookie=strawberry',
+      `PermanentCookie=myPCookie; Max-age=${60*60*24*30}`,
+    ]
   });
   response.end('Cookie!!');
 }).listen(3000);
